09aab9deb11769ddd6d1a95b2607eaefb0cf9517
Tarun Boricha <tarunboricha2092001@gmail.com>
2024-01-04 19:27:09 +0530

    deploy

--- src/app/services/user-ser.service.ts
+++ src/app/services/user-ser.service.ts
@@ -18,14 +18,14 @@ export class UserSerService {
   userSignupservice(data: signUp) {
     data.userID = 0;
     if (data.name != '' && data.email != '' && data.password != '') {
-      this.htttp.post('https://af33-2409-4041-2d80-9c3-6d67-8196-d228-be3a.ngrok-free.app/users',
+      this.htttp.post('https://a1ef-2409-4041-2d80-9c3-817-935f-57de-709c.ngrok-free.app/users',
         data, { headers: this.headers, observe: 'response' }).subscribe((result) => {
           this.router.navigate(['']);
         });
     }
   }
   UserLoginservice(data: Login) {
-    this.htttp.get(`https://af33-2409-4041-2d80-9c3-6d67-8196-d228-be3a.ngrok-free.app/users/${data.email}/${data.password}`,
+    this.htttp.get(`https://a1ef-2409-4041-2d80-9c3-817-935f-57de-709c.ngrok-free.app/users/${data.email}/${data.password}`,
       { headers: this.headers, observe: 'response' }).subscribe((result: any) => {
         console.log(result);
         if (result && result.body && result.body.length) {
