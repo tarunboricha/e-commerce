49320b347aab500e439e160dfcd17837bc5d57e1
Tarun Boricha <tarunboricha2092001@gmail.com>
2024-02-12 17:01:08 +0530

    DEPLOY IT

--- src/app/services/seller-ser.service.ts
+++ src/app/services/seller-ser.service.ts
@@ -26,7 +26,7 @@ export class SellerSerService {
     }
   }
   sellerLoginservice(data: Login) {
-    return this.htttp.get(`https://2384-2409-4041-2615-445d-3042-dd3a-fb94-f97.ngrok-free.app/seller/${data.email}/${data.password}`,
+    return this.htttp.get(`https://a7c2-2409-4041-2615-445d-3042-dd3a-fb94-f97.ngrok-free.app/seller/${data.email}/${data.password}`,
       { headers: this.headers, observe: 'response' });
   }
 }
