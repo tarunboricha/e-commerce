1cabbb759221681afe099e3363eb36f76678b2c5
Tarun Boricha <tarunboricha2092001@gmail.com>
2024-03-09 17:59:00 +0530

    code refector, ui fix

--- src/app/services/seller-ser.service.ts
+++ src/app/services/seller-ser.service.ts
@@ -28,7 +28,7 @@ export class SellerSerService {
   }
 
   sellerLoginservice(data: Login) {
-    return this.htttp.get(`https://5537-103-250-162-216.ngrok-free.app/seller/${data.email}/${data.password}`,
+    return this.htttp.get(`https://553a-103-250-162-216.ngrok-free.app/seller/${data.email}/${data.password}`,
       { headers: this.headers, observe: 'response' });
   }
 }
